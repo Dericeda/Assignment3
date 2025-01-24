68726404c8149d436a005eec10a87f47fa445171
Dericeda <nurasyl.asanov@mail.ru>
2025-01-25 00:48:09 +0500

    Assignment3

---
+++ src/Interfaces/Task16.java
@@ -0,0 +1,18 @@
+package Interfaces;
+
+public class Task16 {
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface Animal {
+        Color getColor();
+
+        Integer getAge();
+    }
+
+     abstract public static class Fox implements  Animal{
+        public String getName() {
+            return "Fox";
+        }
+    }
+}
